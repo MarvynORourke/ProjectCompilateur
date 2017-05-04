ada11faf99b46f77405c554b8065dca36187efc8
MarvynORourke <marvyn81@hotmail.fr>
2017-05-04 18:00:38 +0200

    Partie Gen : DONE. Manque les tests mais ça va le faire

--- gen.ml
+++ gen.ml
@@ -45,7 +45,33 @@ let rec gen_expr liste_var etiquette = function
 																				(a::c)->(gen_expr liste_var etiquette a)@aux(c)
 																				|_->[] in aux list_expr_inverse@[Invoke(tp,name,types_inlist list_expr_inverse)];;
 
+																				
+let rec gen_stmt liste_var etiquette = function
+	(Skip)->[Nop]
+	|(Assign(tp,var,expr))->[Storev(tp,(List.length liste_var)+1)]
+	|(Seq(stmt,stmt1))->(gen_stmt liste_var (1::etiquette) stmt)@(gen_stmt liste_var (2::etiquette) stmt1)
+	|(Cond(expr,stmt,stmt2))->(gen_expr liste_var (1::etiquette) expr)@[Loadc(IntT,IntV 0);If(BCeq,(2::etiquette))]@(gen_stmt liste_var (2::etiquette) stmt)@[Goto(3::etiquette);Label(2::etiquette)]@(gen_stmt liste_var (3::etiquette) stmt)@[Label(3::etiquette)]
+	|(While(expr,stmt))->[Label(1::etiquette)]@(gen_expr liste_var (1::etiquette) expr)@[Loadc(IntT,IntV 0);If(BCeq,(2::etiquette))]@(gen_stmt liste_var (2::etiquette) stmt)@[Goto(1::etiquette);Label(2::etiquette)]
+	|(CallC(name,liste_expr))->let list_expr_inverse = inverse liste_expr in
+																			let rec aux = function 
+																				(a::c)->(gen_expr liste_var etiquette a)@aux(c)
+																				|_->[] in aux list_expr_inverse@[Invoke(VoidT,name,types_inlist list_expr_inverse)]
+	|(Return(expr))->gen_expr liste_var etiquette expr;;
 
+let rec type_list = function
+	((Vardecl(tp,_))::c)->tp::(type_list c)
+	|_->[];;
+	
+let rec name_list = function
+	((Vardecl(_,name))::c)->name::(name_list c)
+	|_->[];;
+	
+let gen_fundefn = function 
+	(Fundefn(Fundecl(tp,name,parameters),local_var,stmt))->[Methdefn(Methdecl(tp,name,type_list parameters),Methinfo(5,5),(gen_stmt (name_list local_var) [0] stmt))];;
+
+
+	
+	
 let env = {localvar = [("n",IntT)]; globalvar = []; returntp = VoidT; funbind = []};;
 
 let expr = BinOp(0,BArith BAadd,(BinOp(0,BArith BAadd,(Const(0,IntV 5)),(VarE(0,Var(Local,"n"))))),(Const(0,IntV 5)));;
