0ba39b78ec5621a350bfddf07a832cbcf6ecaaae
MarvynORourke <marvyn81@hotmail.fr>
2017-05-05 22:25:52 +0200

    fin

--- gen.ml
+++ gen.ml
@@ -28,7 +28,7 @@ let rec types_inlist = function
 
 (*Fonction qui génère le type qui prépare du bytecode*) 
 let rec gen_expr liste_var etiquette = function
-	(Const(tp,c))->[Loadc(tp,c)]
+	(Const(IntT,IntV i))->[Loadc(IntT,IntV i)]
 	| (Const (BoolT, BoolV b)) -> let i = if b then 1 else 0 in [Loadc (IntT, IntV i)]
 	| (Const _) -> failwith "Erreur"
 
@@ -50,23 +50,32 @@ let rec gen_expr liste_var etiquette = function
 				:: [Label lbl_fin]
 		) end	
 	|(IfThenElse(tp,expr1,expr2,expr3))->(gen_expr liste_var (1::etiquette) expr1)@[Loadc(IntT,IntV 0);If(BCeq,(2::etiquette))]@(gen_expr liste_var (2::etiquette) expr2)@[Goto(3::etiquette);Label(2::etiquette)]@(gen_expr liste_var (3::etiquette) expr3)@[Label(3::etiquette)]
-	|(CallE(tp,name,liste_expr))->let list_expr_inverse = inverse liste_expr in 
-																			let rec aux = function 
-																				(a::c)->(gen_expr liste_var etiquette a)@aux(c)
-																				|_->[] in aux list_expr_inverse@[Invoke(tp,name,types_inlist list_expr_inverse)];;
-
-																				
+	| (CallE (tp, fname, exp_lst)) -> 
+		let push_args = List.concat (List.map (gen_expr liste_var etiquette) exp_lst)
+		in let ret_type = b2i tp
+		in let args_tps = List.map b2i (List.map tp_of_expr exp_lst)
+		in push_args @ [Invoke (ret_type, fname, args_tps)]
+	;;																			
 let rec gen_stmt liste_var etiquette = function
 	(Skip)->[Nop]
 	|(Assign(tp,var,expr))->(gen_expr liste_var etiquette expr) @[Storev(tp,(List.length liste_var)+1)]
 	|(Seq(stmt,stmt1))->(gen_stmt liste_var (1::etiquette) stmt)@(gen_stmt liste_var (2::etiquette) stmt1)
-	|(Cond(expr,stmt,stmt2))->(gen_expr liste_var (1::etiquette) expr)@[Loadc(IntT,IntV 0);If(BCeq,(2::etiquette))]@(gen_stmt liste_var (2::etiquette) stmt)@[Goto(3::etiquette);Label(2::etiquette)]@(gen_stmt liste_var (3::etiquette) stmt)@[Label(3::etiquette)]
+	| Cond (cExpr, c1, c2) -> 
+		let lFalse = etiquette @ [0] 
+		and lFin = etiquette @ [2] 
+		in let cond = gen_expr liste_var etiquette cExpr
+		and t = gen_stmt liste_var etiquette c1
+		and f = gen_stmt liste_var etiquette c2
+		in
+			cond @ [Loadc (IntT, IntV 0); If (BCeq, lFalse)] @
+			t @ [Goto lFin; Label lFalse] @ 
+			f @ [Label lFin]
 	|(While(expr,stmt))->[Label(1::etiquette)]@(gen_expr liste_var (1::etiquette) expr)@[Loadc(IntT,IntV 0);If(BCeq,(2::etiquette))]@(gen_stmt liste_var (2::etiquette) stmt)@[Goto(1::etiquette);Label(2::etiquette)]
 	|(CallC(name,liste_expr))->let list_expr_inverse = inverse liste_expr in
 																			let rec aux = function 
 																				(a::c)->(gen_expr liste_var etiquette a)@aux(c)
 																				|_->[] in aux list_expr_inverse@[Invoke(VoidT,name,types_inlist list_expr_inverse)]
-	| Return rExpr -> [ReturnI (b2i (Lang.tp_of_expr rExpr))];;
+	| Return expr -> [ReturnI (b2i (Lang.tp_of_expr expr))];;
 
 let rec type_list = function
 	((Vardecl(tp,_))::c)->tp::(type_list c)
@@ -77,7 +86,7 @@ let rec name_list = function
 	|_->[];;
 	
 let gen_fundefn = function 
-	(Fundefn(Fundecl(tp,name,parameters),local_var,stmt))->Methdefn(Methdecl(b2i tp,name,type_list parameters),Methinfo((List.length local_var),(stack_depth_c stmt) +1),(gen_stmt ((name_list parameters)@(name_list local_var)) [0] stmt));;
+	(Fundefn(Fundecl(tp,name,parameters),local_var,stmt))->Methdefn(Methdecl(b2i tp,name,type_list parameters),Methinfo((stack_depth_c stmt) +1,(List.length local_var)),(gen_stmt ((name_list parameters)@(name_list local_var)) [] stmt));;
 	
 let gen_prog (Prog (gvds, fdfs)) = 
   JVMProg ([],List.map gen_fundefn fdfs);;
